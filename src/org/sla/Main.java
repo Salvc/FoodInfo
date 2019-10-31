10514e63d02ceedbe235eade3803e5c710646fbe
Salvc <scamarote20@scienceleadership.org>
2019-10-31 10:01:32 -0400

    Initial commit

---
+++ src/org/sla/Main.java
@@ -0,0 +1,16 @@
+package org.sla;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+
+
+
+
+
+
+        System.out.println("Sal says: Let's get to work Nicco.  No more game and watch image searches.");
+    }
+}
