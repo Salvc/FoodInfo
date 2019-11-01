f44f7025d63e194c1f3b054d97438debe801a4cb
Salvc <scamarote20@scienceleadership.org>
2019-11-01 13:13:49 -0400

    Fast food is gettin ter

--- src/org/sla/FastFoods.java
+++ src/org/sla/FastFoods.java
@@ -1,4 +1,51 @@
 package org.sla;
 
-public class FastFoods {
-}
+ class FastFoods extends Food{
+
+     private int transFat;
+     private int saturatedFat;
+     private String chainName;
+     private String foodType;
+
+     public FastFoods(String name, int servingSize, int calories, int fat, int sodium, int carbs, int transFat, int saturatedFat, String chainName, String foodType){
+         super(name,servingSize,calories,fat,sodium,carbs);
+
+         this.transFat = transFat;
+         this.saturatedFat = saturatedFat;
+         this.chainName = chainName;
+         this.foodType = foodType;
+
+     }
+
+     public int getTransFat() {
+         return transFat;
+     }
+
+     public void setTransFat(int transFat) {
+         this.transFat = transFat;
+     }
+
+     public int getSaturatedFat() {
+         return saturatedFat;
+     }
+
+     public void setSaturatedFat(int saturatedFat) {
+         this.saturatedFat = saturatedFat;
+     }
+
+     public String getChainName() {
+         return chainName;
+     }
+
+     public void setChainName(String chainName) {
+         this.chainName = chainName;
+     }
+
+     public String getFoodType() {
+         return foodType;
+     }
+
+     public void setFoodType(String foodType) {
+         this.foodType = foodType;
+     }
+ }
