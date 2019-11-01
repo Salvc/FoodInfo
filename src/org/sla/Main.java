1c34d72d4ea1e7a718b291a09f23df91916d05d8
W1derfire <niccobeanz@gmail.com>
2019-11-01 13:33:02 -0400

    Added the file "fruits"

--- src/org/sla/Main.java
+++ src/org/sla/Main.java
@@ -5,8 +5,6 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
 
-        Food burger = new Food("Burger",1,1,1,1,1);
-
         System.out.println("Sal says: Let's get to work Nicco.  No more game and watch image searches.");
 
         System.out.println("Nicco: That's a lot coming from a Pokemon trainer. I will edge-guard you to death.");
